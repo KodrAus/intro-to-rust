a3d9173e684272bea0210c8ffed4c7a03d11e5c7
Ashley Mannix <ashleymannix@live.com.au>
2016-08-20 11:47:57 +1000

    add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Intro to Rust
+
+This is the repo for my introduction to Rust talk.
+It's got all the lazy, dodgy markup you could hope for :)
+
+I'll work on improving it in the future.
--- src/main.rs
+++ src/main.rs
@@ -16,36 +16,36 @@ use hbs::{ Template, HandlebarsEngine, DirectorySource };
 fn main() {
 	let mut hbse = HandlebarsEngine::new();
 
-    // add a directory source, all files with .hbs suffix will be loaded as template
+	// add a directory source, all files with .hbs suffix will be loaded as template
 	hbse.add(Box::new(DirectorySource::new("./src/slides/", ".hbs")));
 	hbse.reload().unwrap();
 
-    let mut router = Router::new();
-    router.get("/", index)
-    	  .get("*", render);
+	let mut router = Router::new();
+	router.get("/", index)
+		  .get("*", render);
 
-    let mut mount = Mount::new();
-    mount.mount("/", router)
-         .mount("/assets/", Static::new(Path::new("./src/assets/")));
+	let mut mount = Mount::new();
+	mount.mount("/", router)
+		 .mount("/assets/", Static::new(Path::new("./src/assets/")));
 
-    let mut chain = Chain::new(mount);
-    chain.link_after(hbse);
+	let mut chain = Chain::new(mount);
+	chain.link_after(hbse);
 
 	Iron::new(chain).http("localhost:3000").unwrap();
 }
 
 //Render the index page 'irregardless' of the base route
 fn index(_: &mut Request) -> IronResult<Response> {
-    let mut resp = Response::new();
-    resp.set_mut(Template::new("index", Some(()))).set_mut(status::Ok);
-    Ok(resp)
+	let mut resp = Response::new();
+	resp.set_mut(Template::new("index", Some(()))).set_mut(status::Ok);
+	Ok(resp)
 }
 
 ///Render the requested slide
 fn render(r: &mut Request) -> IronResult<Response> {
 	let path = r.url.path().join("/");
 
-    let mut resp = Response::new();
-    resp.set_mut(Template::new(&path, Some(()))).set_mut(status::Ok);
-    Ok(resp)
+	let mut resp = Response::new();
+	resp.set_mut(Template::new(&path, Some(()))).set_mut(status::Ok);
+	Ok(resp)
 }
